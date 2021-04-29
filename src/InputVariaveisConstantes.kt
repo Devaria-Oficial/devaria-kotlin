fa84733ef009266e29b82960e946e1c4ce379ecb
Rafael Thomazelli <rafael@verzel.com.br>
2021-04-28 21:00:34 -0300

    conclusao parte 1 e 2 de Kotlin

---
+++ src/InputVariaveisConstantes.kt
@@ -0,0 +1,20 @@
+fun main(argumentos : Array<String>) {
+
+    if(argumentos.isEmpty()) {
+        println("Favor informar um argumento válido")
+        return
+    }
+
+    val numero = argumentos[0].toIntOrNull()
+
+    if(numero == null){
+        println("O Valor informado não é um número válido!")
+        return
+    }
+
+    println("o número digitado foi: $numero")
+
+    var soma = 0
+    soma = numero + numero
+    println("A soma do número + número é de: $soma")
+}
---
+++ src/OlaMundo.kt
@@ -0,0 +1,3 @@
+fun main() {
+    print("Olá mundo, meu primeiro código Kotlin")
+}
